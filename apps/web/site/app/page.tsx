7b3ad06c56cfb05c17cf6d9c660ab0a5682bd5b2
CUSTcoding <custodiotitossetitosse@gmail.com>
2026-01-14 17:52:01 -0600

    feat(web): add official site folder

---
+++ apps/web/site/app/page.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/components/footer/footer";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Footer />
+    </div>
+  );
+}
---
+++ apps/web/site/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <div className=" w-full bg-amber-500 items-center justify-center p-20 flex">
+            <h2 className="text-blue-50 font-bold">luff</h2>
+        </div>
+    );
+}
